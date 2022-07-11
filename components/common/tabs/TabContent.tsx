6aae961609b552226b607f9e08c0f5aa8c125ff7
asantech <moayeri68@gmail.com>
2022-07-11 13:15:14 +0430

    dottedSpinner removed from tab content

--- components/common/tabs/TabContent.tsx
+++ components/common/tabs/TabContent.tsx
@@ -1,19 +1,16 @@
 import TabPane from './TabPane';
-import DottedSpinner from 'components/common/spinners/DottedSpinner';
 
 import styles from './TabContent.module.scss';
 
 function TabContent(props: any) {
-  const { showSpinner, tabs } = props;
+  const { tabs } = props;
   return (
     <div
       className={styles['tab-content'] + ' tab-content position-relative mb-3'}
     >
-      {showSpinner && <DottedSpinner />}
-      {!showSpinner &&
-        tabs.map((tab: any, i: number) => (
-          <TabPane key={i} tab={tab}></TabPane>
-        ))}
+      {tabs.map((tab: any, i: number) => (
+        <TabPane key={i} tab={tab}></TabPane>
+      ))}
     </div>
   );
 }
